6bf46e2e8bcd0eceb4453c37dd2c89ad8e6ba553
Raist <laremars@vols.utk.edu>
2018-07-05 07:36:45 -0400

    uploading changes over 4th

--- tiny/app.py
+++ tiny/app.py
@@ -7,32 +7,45 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_unbreakable_key_yo'
 
 class Login(FlaskForm):
-    name = StringField('Enter Name', validators=[DataRequired()])
+    name1 = StringField('Enter Name', validators=[DataRequired()], render_kw={'autofocus': True})
     submit1 = SubmitField('Login')
     
 class Register(FlaskForm):
-    name = StringField('Enter Name', validators=[DataRequired()])
+    name2 = StringField('Enter Name', validators=[DataRequired()], render_kw={'autofocus': True})
     submit2 = SubmitField('Register')
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def home():
+    name_exists = True
     form1 = Login()
     form2 = Register()
     #if request.method == "POST":
     if form1.validate_on_submit() and form1.submit1.data:
-        name=form1.name.data
-        flash("Welcome back,  "+name, 'success')
-        flash("Would you like to see your most recent queries?", 'info')
-        return redirect(url_for('home'))
+        name=form1.name1.data
+        if name_exists:#simulates querying the db
+            flash("Welcome back,  "+name, 'success')
+            return render_template('index.html', form1=form1, form2=form2, name=name)
+        else:
+            flash("The name, "+name+", is not reflected in the database. Please Do Better. ", 'warning')
+            return render_template('index.html', form1=form1, form2=form2)
     if form2.validate_on_submit() and form2.submit2.data:
-        name=form2.name.data
-        flash("Registration Successful", 'success')
-        flash("This username, "+name+", will henceforth persist allowing cached historical queries to be more conveniently rendered.", 'info')
-        return redirect(url_for('home'))
+        #Register user in db
+        name=form2.name2.data
+        name_not_taken = True
+        if name_not_taken:#simulates querying the db to check against existing names
+            flash("Registration Successful", 'success')
+            flash("This username, "+name+", will henceforth persist allowing cached historical queries to be more conveniently rendered.", 'info')
+            return render_template('index.html', form1=form1, form2=form2, name=name, first_time=True)
     
     return render_template('index.html', form1=form1, form2=form2)
+
+@app.route("/logged_in", methods=['GET', 'POST'])
+def logged_in():
+    name='Larry'#find name in session
+    return render_template('logged_in.html')
+
 
 @app.route("/macros")
 def macros():
--- tiny/static/js/scripts.js
+++ tiny/static/js/scripts.js
@@ -3,7 +3,26 @@ $("document").ready(function() {
     /*$( function() {//Test JQuery-UI functioning
         $( "#draggable" ).draggable();
     } );*/
+    $(".form1").slideToggle();
+    $(".form2").slideToggle();
+    
+    $("button").click(function(){
+        val = $(this).attr("value");
+        if (val == "Login") {
+            $(".form1").removeClass("invisible").slideToggle("fast");
+            $("#name1").focus();
+            
+        } else if (val == "Register") {
+            $(".form2").slideToggle("fast").removeClass("invisible");
+            $("#name2").focus();
+        } else {
+            console.log("Button value undefined");
+        }
+        //console.log(typeof val);
+        //$(".append").append(" "+step1 +"<br><b>Selection Criterea:</b> ");
+    });
     
     
+
     
-});
+});//$("document").ready(function() {
